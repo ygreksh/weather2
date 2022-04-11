ba70afe6a970cd982a27ebb480d98968bbbaaf91
ygrek <ygreksh@gmail.com>
2022-04-11 09:31:23 +0300

    double useisFocused fixed

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,7 +3,6 @@ import {Text, Button, View, Image, Alert} from 'react-native';
 import { useIsFocused } from '@react-navigation/native';
 import { CityList, WeatherItem, WeatherList } from '../components';
 import Geolocation from '@react-native-community/geolocation';
-import { useIsFocused } from '@react-navigation/native';
 import {useSelectedCityStore, useCurrentWeatherStore, useMyLocationStore, useMyCityStore, useMyCityListStore} from '../store';
 import { apiService } from '../services/api';
 
